026d0ab6aeca685f603bf7b8da63d696e29e6db3
CristianoSanto <crrs008@gmail.com>
2022-03-20 22:30:28 -0300

    Novo Commit

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 
+console.log(`A velocida do carro é de ${vel} Km por hora.`)
+if (vel > 80) {
+    console.log('Sorria você foi multado')
+
+}
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = ('Brasil')
+if (pais == 'Brasil') {
+    console.log('Cabron você é Brasileiro')
+}
+else {
+    console.log('You is Stranger')
+}
